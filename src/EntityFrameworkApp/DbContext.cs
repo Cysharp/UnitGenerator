72638070af7c1cb91f1fe773a808130cb242b906
spacekey <fumihiro.fukuda@gmail.com>
2022-10-25 09:37:17 +0900

    Add parameter less constructor to EntityFrameworkValueConverter

---
+++ src/EntityFrameworkApp/DbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UnitGenerator;
+
+namespace EntityFrameworkApp;
+
+[UnitOf(typeof(int),
+    UnitGenerateOptions.ParseMethod |
+    UnitGenerateOptions.EntityFrameworkValueConverter)]
+public readonly partial struct UserId { }
+
+public class User
+{
+    public UserId UserId { get; set; }
+}
+
+public class SampleDbContext : DbContext
+{
+    public SampleDbContext() { } 
+    public SampleDbContext(DbContextOptions<SampleDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; } = default!;
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<UserId>().HaveConversion<UserId.UserIdValueConverter>();
+    }
+}

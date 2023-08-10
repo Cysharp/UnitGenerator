5a97cd1870bad302a53a20b1f8fa3287df9df826
hadashiA <dev@hadashikick.jp>
2023-08-10 16:07:37 +0900

    Add options to define arithmetic operators separately

--- src/UnitGenerator/SourceGenerator.cs
+++ src/UnitGenerator/SourceGenerator.cs
@@ -133,19 +133,25 @@ namespace UnitGenerator
     internal enum UnitGenerateOptions
     {
         None = 0,
-        ImplicitOperator = 1,
-        ParseMethod = 2,
-        MinMaxMethod = 4,
-        ArithmeticOperator = 8,
-        ValueArithmeticOperator = 16,
-        Comparable = 32,
-        Validate = 64,
-        JsonConverter = 128,
-        MessagePackFormatter = 256,
-        DapperTypeHandler = 512,
-        EntityFrameworkValueConverter = 1024,
-        WithoutComparisonOperator = 2048,
-        JsonConverterDictionaryKeySupport = 4096
+        ImplicitOperator = 1,    
+        ParseMethod = 1 << 1,
+        MinMaxMethod = 1 << 2,
+        ArithmeticOperator = 1 << 3,
+        ValueArithmeticOperator = 1 << 4,
+        Comparable = 1 << 5,
+        Validate = 1 << 6,
+        JsonConverter = 1 << 7,
+        MessagePackFormatter = 1 << 8,
+        DapperTypeHandler = 1 << 9,
+        EntityFrameworkValueConverter = 1 << 10,
+        WithoutComparisonOperator = 1 << 11,
+        JsonConverterDictionaryKeySupport = 1 << 12,
+        ArithmeticOperatorAddition = 1 << 13,
+        ArithmeticOperatorSubtraction = 1 << 13,
+        ArithmeticOperatorMultiply = 1 << 14,
+        ArithmeticOperatorDivision = 1 << 15,
+        ValueArithmeticOperatorAddition = 1 << 16,
+        ValueArithmeticOperatorSubtraction = 1 << 17    
     }
 }
 """;
@@ -439,11 +445,13 @@ namespace {{ns}}
  """);
             }
 
-            if (prop.HasFlag(UnitGenerateOptions.ArithmeticOperator))
+            if (prop.HasFlag(UnitGenerateOptions.ArithmeticOperator) ||
+                prop.HasFlag(UnitGenerateOptions.ArithmeticOperatorAddition) ||
+                prop.HasFlag(UnitGenerateOptions.ValueArithmeticOperator) ||
+                prop.HasFlag(UnitGenerateOptions.ValueArithmeticOperatorAddition))
             {
                 sb.AppendLine($$"""
-        // UnitGenerateOptions.ArithmeticOperator
-
+        // UnitGenerateOptions.ArithmeticOperator (Addition)
         public static {{unitTypeName}} operator +(in {{unitTypeName}} x, in {{unitTypeName}} y)
         {
             checked
@@ -451,7 +459,17 @@ namespace {{ns}}
                 return new {{unitTypeName}}(({{innerTypeName}})(x.value + y.value));
             }
         }
-
+                                
+""");
+            }
+            
+            if (prop.HasFlag(UnitGenerateOptions.ArithmeticOperator) ||
+                prop.HasFlag(UnitGenerateOptions.ArithmeticOperatorSubtraction) ||
+                prop.HasFlag(UnitGenerateOptions.ValueArithmeticOperator) ||
+                prop.HasFlag(UnitGenerateOptions.ValueArithmeticOperatorSubtraction))
+            {
+                sb.AppendLine($$"""
+        // UnitGenerateOptions.ArithmeticOperator (Subtraction)
         public static {{unitTypeName}} operator -(in {{unitTypeName}} x, in {{unitTypeName}} y)
         {
             checked
@@ -459,7 +477,16 @@ namespace {{ns}}
                 return new {{unitTypeName}}(({{innerTypeName}})(x.value - y.value));
             }
         }
-
+                                
+""");
+            }
+            
+            if (prop.HasFlag(UnitGenerateOptions.ArithmeticOperator) ||
+                prop.HasFlag(UnitGenerateOptions.ArithmeticOperatorMultiply) ||
+                prop.HasFlag(UnitGenerateOptions.ValueArithmeticOperator))
+            {
+                sb.AppendLine($$"""
+        // UnitGenerateOptions.ArithmeticOperator (Multiply)
         public static {{unitTypeName}} operator *(in {{unitTypeName}} x, in {{unitTypeName}} y)
         {
             checked
@@ -467,7 +494,16 @@ namespace {{ns}}
                 return new {{unitTypeName}}(({{innerTypeName}})(x.value * y.value));
             }
         }
-
+                                
+""");
+            }
+            
+            if (prop.HasFlag(UnitGenerateOptions.ArithmeticOperator) ||
+                prop.HasFlag(UnitGenerateOptions.ArithmeticOperatorDivision) ||
+                prop.HasFlag(UnitGenerateOptions.ValueArithmeticOperator))
+            {
+                sb.AppendLine($$"""
+        // UnitGenerateOptions.ArithmeticOperator (Division)
         public static {{unitTypeName}} operator /(in {{unitTypeName}} x, in {{unitTypeName}} y)
         {
             checked
@@ -479,11 +515,11 @@ namespace {{ns}}
 """);
             }
 
-            if (prop.HasFlag(UnitGenerateOptions.ValueArithmeticOperator))
+            if (prop.HasFlag(UnitGenerateOptions.ValueArithmeticOperator) || 
+                prop.HasFlag(UnitGenerateOptions.ValueArithmeticOperatorAddition))
             {
                 sb.AppendLine($$"""
-        // UnitGenerateOptions.ValueArithmeticOperator
-
+        // UnitGenerateOptions.ValueArithmeticOperator (Addition)
         public static {{unitTypeName}} operator ++(in {{unitTypeName}} x)
         {
             checked
@@ -492,43 +528,19 @@ namespace {{ns}}
             }
         }
 
+""");
+            }
+
+            if (prop.HasFlag(UnitGenerateOptions.ValueArithmeticOperator) || 
+                prop.HasFlag(UnitGenerateOptions.ValueArithmeticOperatorSubtraction))
+            {
+                sb.AppendLine($$"""
+        // UnitGenerateOptions.ValueArithmeticOperator (Subtraction)
         public static {{unitTypeName}} operator --(in {{unitTypeName}} x)
         {
             checked
             {
                 return new {{unitTypeName}}(({{innerTypeName}})(x.value - 1));
-            }
-        }
-
-        public static {{unitTypeName}} operator +(in {{unitTypeName}} x, in {{innerTypeName}} y)
-        {
-            checked
-            {
-                return new {{unitTypeName}}(({{innerTypeName}})(x.value + y));
-            }
-        }
-
-        public static {{unitTypeName}} operator -(in {{unitTypeName}} x, in {{innerTypeName}} y)
-        {
-            checked
-            {
-                return new {{unitTypeName}}(({{innerTypeName}})(x.value - y));
-            }
-        }
-
-        public static {{unitTypeName}} operator *(in {{unitTypeName}} x, in {{innerTypeName}} y)
-        {
-            checked
-            {
-                return new {{unitTypeName}}(({{innerTypeName}})(x.value * y));
-            }
-        }
-
-        public static {{unitTypeName}} operator /(in {{unitTypeName}} x, in {{innerTypeName}} y)
-        {
-            checked
-            {
-                return new {{unitTypeName}}(({{innerTypeName}})(x.value / y));
             }
         }
 
--- src/UnitGenerator/UnitGenerateOptions.cs
+++ src/UnitGenerator/UnitGenerateOptions.cs
@@ -7,18 +7,24 @@ namespace UnitGenerator
     internal enum UnitGenerateOptions
     {
         None = 0,
-        ImplicitOperator = 1,
-        ParseMethod = 2,
-        MinMaxMethod = 4,
-        ArithmeticOperator = 8,
-        ValueArithmeticOperator = 16,
-        Comparable = 32,
-        Validate = 64,
-        JsonConverter = 128,
-        MessagePackFormatter = 256,
-        DapperTypeHandler = 512,
-        EntityFrameworkValueConverter = 1024,
-        WithoutComparisonOperator = 2048,
-        JsonConverterDictionaryKeySupport = 4096
+        ImplicitOperator = 1,        
+        ParseMethod = 1 << 1,
+        MinMaxMethod = 1 << 2,
+        ArithmeticOperator = 1 << 3,
+        ValueArithmeticOperator = 1 << 4,
+        Comparable = 1 << 5,
+        Validate = 1 << 6,
+        JsonConverter = 1 << 7,
+        MessagePackFormatter = 1 << 8,
+        DapperTypeHandler = 1 << 9,
+        EntityFrameworkValueConverter = 1 << 10,
+        WithoutComparisonOperator = 1 << 11,
+        JsonConverterDictionaryKeySupport = 1 << 12,
+        ArithmeticOperatorAddition = 1 << 13,
+        ArithmeticOperatorSubtraction = 1 << 13,
+        ArithmeticOperatorMultiply = 1 << 14,
+        ArithmeticOperatorDivision = 1 << 15,
+        ValueArithmeticOperatorAddition = 1 << 16,
+        ValueArithmeticOperatorSubtraction = 1 << 17
     }
 }
